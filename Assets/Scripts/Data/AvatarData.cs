c08fb420cd2d9c8cfb87181b77f73150386e6b0f
Виталий <efiop80@gmail.com>
2024-11-01 15:37:32 +0700

    add new data
    добавляю данные для аватаров что бы можно было каждому аварату добавить время анимации в кадрах в секунду. Так же нужно сделать что бы внутри папок состояний чекались другие папки. Если внутри папки состояния нет папок но есть файлы .png то имеем только одну анимацию состояния. Если внутри папки состояния есть дополнительные папки с файлами .png, то кол-во анимаций этого состояния равно кол-ву этих папок + (если) в самой папке состояния есть файлы .png без папки. Допустим у состояния Idle внутри 2 папки с .png файлами: stay и dance. Значит состояние idle имеет 2 анимации, которые выбираются случайно при приобретении аватаром состояния Idle.

---
+++ Assets/Scripts/Data/AvatarData.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace Data
+{
+    public class AvatarData
+    {
+        [JsonProperty("n")] public string AvatarName;
+        [JsonProperty("s")] public float Speed;
+        
+        [JsonProperty("a")] public int Access = 0;
+
+        [JsonProperty("aa")] [CanBeNull] public AvatarAnimationData[] AttackAnimationsIndices;
+        [JsonProperty("ia")] [CanBeNull] public AvatarAnimationData[] IdleAnimationsIndices;
+        [JsonProperty("la")] [CanBeNull] public AvatarAnimationData[] LeftAnimationsIndices;
+        [JsonProperty("ra")] [CanBeNull] public AvatarAnimationData[] RightAnimationsIndices;
+    }
+
+    public class AvatarAnimationData
+    {
+        [JsonProperty("fps")] public float FramePerSecond;
+        [JsonProperty("ai")] public int[] AnimationIndices;
+    }
+}
---
+++ Assets/Scripts/Data/ChatterStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data
+{
+    [Flags]
+    public enum ChatterStatus
+    {
+        //  0 - for all chatters
+        Subscriber = 1,
+        Vip = 2,
+        Moderator = 4
+    }
+}
